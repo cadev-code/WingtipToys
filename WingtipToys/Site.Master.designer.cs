be35817dc1b43705e63fb4789381bcbece08a660
cadev-code <carlosdev1810@gmail.com>
2026-04-19 00:23:46 -0600

    Añadir menú de categorías con ListView en Site.Master
    Se ha incorporado un menú de categorías en Site.Master usando un control ListView (categoryList) que enlaza a las páginas de productos por categoría. También se ha actualizado Site.Master.designer.cs para declarar el nuevo control.

--- WingtipToys/Site.Master.designer.cs
+++ WingtipToys/Site.Master.designer.cs
@@ -24,6 +24,15 @@ namespace WingtipToys
         protected global::System.Web.UI.WebControls.Image Image1;
 
         /// <summary>
+        /// Control categoryList.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListView categoryList;
+
+        /// <summary>
         /// Control MainContent.
         /// </summary>
         /// <remarks>
